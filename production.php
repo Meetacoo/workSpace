3289d131f09869ad7d9bb6793680f73f8f082666
HJF <2283785225@qq.com>
2019-02-20 18:05:40 +0800

    更换路径

---
+++ production.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Template Name: 迪付产品模板
+ */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title -->
+    <title>迪付产品 - 上海迪付金融有限公司</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="http://localhost/wordpress/wp-content/themes/difu-web/assets/img/core-img/favicon.ico">
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="http://localhost/wordpress/wp-content/themes/difu-web/assets/css/style.css">
+    <link rel="stylesheet" href="http://localhost/wordpress/wp-content/themes/difu-web/assets/css/common.css">
+    <link rel="stylesheet" href="http://localhost/wordpress/wp-content/themes/difu-web/assets/css/container/index.css">
+    <link rel="stylesheet" href="http://localhost/wordpress/wp-content/themes/difu-web/assets/css/container/production.css">
+
+</head>
+
+<body>
+<!-- Preloader -->
+<div id="preloader"></div>
+<!-- ##### Header Area Start ##### -->
+<div id="header"></div>
+<!-- ##### Header Area End ##### -->
+
+<!-- ##### Breadcrumb Area Start ##### -->
+<div id="productionBanner"></div>
+<!-- ##### Breadcrumb Area End ##### -->
+<!-- ##### Services Area Start ###### -->
+<div id="core"></div>
+<!-- ##### Services Area End ###### -->
+<!-- service-area start -->
+<div id="production"></div>
+<!-- service-area end -->
+<!-- ##### Newsletter Area Start ###### -->
+<div id="productionLink"></div>
+<!-- ##### Newsletter Area End ###### -->
+
+<!-- ##### Footer Area Start ##### -->
+<div id="footer"></div>
+<!-- ##### Footer Area Start ##### -->
+
+<!-- ##### All Javascript Script ##### -->
+<!-- jQuery-2.2.4 js -->
+<script src="http://localhost/wordpress/wp-content/themes/difu-web/assets/js/jquery/jquery-2.2.4.min.js"></script>
+<script src="http://localhost/wordpress/wp-content/themes/difu-web/assets/utils/template.js"></script>
+<script src="http://localhost/wordpress/wp-content/themes/difu-web/assets/component/Preloader.js"></script>
+<script src="http://localhost/wordpress/wp-content/themes/difu-web/assets/component/Header.js"></script>
+<script src="http://localhost/wordpress/wp-content/themes/difu-web/assets/component/HeaderBanner.js"></script>
+<script src="http://localhost/wordpress/wp-content/themes/difu-web/assets/component/WelcomeBanner.js"></script>
+<script src="http://localhost/wordpress/wp-content/themes/difu-web/assets/component/Footer/index.js"></script>
+<script src="http://localhost/wordpress/wp-content/themes/difu-web/assets/component/Core.js"></script>
+<script src="http://localhost/wordpress/wp-content/themes/difu-web/assets/component/production.js"></script>
+<script src="http://localhost/wordpress/wp-content/themes/difu-web/assets/js/container/common.js"></script>
+<script src="http://localhost/wordpress/wp-content/themes/difu-web/assets/js/container/production.js"></script>
+<!-- Popper js -->
+<script src="http://localhost/wordpress/wp-content/themes/difu-web/assets/js/bootstrap/popper.min.js"></script>
+<!-- Bootstrap js -->
+<script src="http://localhost/wordpress/wp-content/themes/difu-web/assets/js/bootstrap/bootstrap.min.js"></script>
+<!-- All Plugins js -->
+<script src="http://localhost/wordpress/wp-content/themes/difu-web/assets/js/plugins/plugins.js"></script>
+<!-- Active js -->
+<script src="http://localhost/wordpress/wp-content/themes/difu-web/assets/js/active.js"></script>
+</body>
+
+</html>
+
+
