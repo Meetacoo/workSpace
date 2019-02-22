436e5b2c731a75dacca350e069dd66d7132cae97
HJF <2283785225@qq.com>
2019-02-22 10:57:52 +0800

    更改路径

---
+++ assets/template/welcome/indexWelcome.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: 黄金芳
+ * Date: 2019/2/22
+ * Time: 10:30
+ */
+?>
+<?php
+$args = array(
+    'title' => '加入我们',
+    'linkTo' => 'http://localhost/wordpress/joinUs',
+    'button' => '申请加入',
+    'description' => '你还在犹豫什么？加入我们，享受豪华套餐。'
+);
+welcomeTemplate($args);
+?>
--- index.php
+++ index.php
@@ -215,7 +215,7 @@ get_template_part('assets/template/indexCore','template');
 <?php
 get_template_part('assets/template/indexProduction','template');
 get_template_part('assets/template/partner','template');
-get_template_part('assets/template/indexWelcome');
+get_template_part('assets/template/welcome/indexWelcome');
 get_template_part('assets/template/footer','template');
 ?>
 <!-- ##### Footer Area Start ##### -->
