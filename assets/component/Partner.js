75ef127879154fdeb5ea559de388338c2ce1d909
HJF <2283785225@qq.com>
2019-02-22 10:21:14 +0800

    合作伙伴动画

--- assets/component/Partner.js
+++ assets/component/Partner.js
@@ -3,59 +3,21 @@
  * author: yangyao(873241789@qq.com)
  * date: 2019/2/12
  */
-
-/**
- * 合作伙伴 字符串模版
- * @param dataSource
- * @returns {string}
- */
-function partnerInnerHtml(dataSource) {
-    var Default_Html = '';
-    for (var i = 0; i < dataSource.length; i++) {
-        Default_Html += '<div class="single-logo-wrapper">\n' +
-            '                        <div class="single-item">\n' +
-            '                            <img src="' + dataSource[i].src + '" alt="">\n' +
-            '                        </div>\n' +
-            '            </div>';
+$(".logo-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    margin: 30,
+    autoplay: true,
+    autoplayspeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 5
+        }
     }
-    return '<div class="about-area">\n' +
-        '    <div class="space-30"></div>\n' +
-        '    <div>\n' +
-        '        <div class="row">\n' +
-        '            <div class="col-12 text-center">\n' +
-        '                <div class="heading">\n' +
-        '                    <h5>海量金融客户选择迪付</h5>\n' +
-        '                </div>\n' +
-        '                <div class="space-30"></div>\n' +
-        '            </div>\n' +
-        '        </div>\n' +
-        '        <div class="row box-model">\n' +
-        '            <div class="col-lg-12">\n' +
-        '                <div class="logo-carousel owl-carousel text-center">' + Default_Html + ' </div>\n' +
-        '            </div>\n' +
-        '        </div>\n' +
-        '    </div>\n' +
-        '</div>\n'
-}
-
-/**
- * 导出 合作伙伴模版
- * @returns {*}
- * @constructor
- */
-function PartnerTemplate() {
-    var config = [{
-        src: 'http://localhost/wordpress/wp-content/themes/difu-web/assets/img/logo/c-logo-1.png'
-    }, {
-        src: 'http://localhost/wordpress/wp-content/themes/difu-web/assets/img/logo/c-logo-2.png'
-    }, {
-        src: 'http://localhost/wordpress/wp-content/themes/difu-web/assets/img/logo/c-logo-3.png'
-    }, {
-        src: 'http://localhost/wordpress/wp-content/themes/difu-web/assets/img/logo/c-logo-4.png'
-    }, {
-        src: 'http://localhost/wordpress/wp-content/themes/difu-web/assets/img/logo/c-logo-5.png'
-    }, {
-        src: 'http://localhost/wordpress/wp-content/themes/difu-web/assets/img/logo/c-logo-1.png'
-    }];
-    return generateTemplate(partnerInnerHtml(config));
-}
+});
