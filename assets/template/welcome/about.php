99993af76e56e540b8f7e6b68c09cf845d718605
HJF <2283785225@qq.com>
2019-02-22 15:54:29 +0800

    footerBanner

--- assets/template/welcome/about.php
+++ assets/template/welcome/about.php
@@ -3,13 +3,13 @@
  * Created by IntelliJ IDEA.
  * User: 黄金芳
  * Date: 2019/2/22
- * Time: 10:30
+ * Time: 11:15
  */
 ?>
 <?php
 $args = array(
-    'title' => '加入我们',
-    'linkTo' => 'http://localhost/wordpress/joinUs',
+    'title' => '想要加入我们吗',
+    'linkTo' => 'https://www.lagou.com/gongsi/j171527.html#company_navs',
     'button' => '申请加入',
     'description' => '你还在犹豫什么？加入我们，享受豪华套餐。'
 );
