aa33d6ca32a94e50adba42968f0c2d307725b634
HJF <2283785225@qq.com>
2019-02-22 18:45:51 +0800

    模块化team

--- about.php
+++ about.php
@@ -255,92 +255,9 @@ get_template_part('assets/template/banner/about');
 <!--roadmap area end-->
 
 <!-- ##### Team Member Area Start ##### -->
-<section class="team-area section-padding-100-0">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <!-- Section Heading -->
-                <div class="section-heading text-center mb-100">
-                    <div class="line"></div>
-                    <p>Our team</p>
-                    <h2>我们的团队</h2>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-
-            <!-- Single Team Member Area -->
-            <div class="col-12 col-sm-6 col-lg-3">
-                <div class="single-team-member-area mb-100">
-                    <div class="team-thumb">
-                        <img src="<?php echo get_template_directory_uri().'/assets/img/bg-img/15.jpg'; ?>'; ?>" alt="">
-                        <!-- View More -->
-                        <div class="view-more">
-                            <a href="#">+</a>
-                        </div>
-                    </div>
-                    <div class="team-info">
-                        <h5>王婷婷1</h5>
-                        <h6>工业设计副总裁2016年</h6>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Single Team Member Area -->
-            <div class="col-12 col-sm-6 col-lg-3">
-                <div class="single-team-member-area mb-100">
-                    <div class="team-thumb">
-                        <img src="<?php echo get_template_directory_uri().'/assets/img/bg-img/16.jpg'; ?>'; ?>" alt="">
-                        <!-- View More -->
-                        <div class="view-more">
-                            <a href="#">+</a>
-                        </div>
-                    </div>
-                    <div class="team-info">
-                        <h5>王婷婷2</h5>
-                        <h6>工业设计副总裁2017年</h6>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Single Team Member Area -->
-            <div class="col-12 col-sm-6 col-lg-3">
-                <div class="single-team-member-area mb-100">
-                    <div class="team-thumb">
-                        <img src="<?php echo get_template_directory_uri().'/assets/img/bg-img/17.jpg'; ?>'; ?>" alt="">
-                        <!-- View More -->
-                        <div class="view-more">
-                            <a href="#">+</a>
-                        </div>
-                    </div>
-                    <div class="team-info">
-                        <h5>王婷婷3</h5>
-                        <h6>工业设计副总裁2018年</h6>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Single Team Member Area -->
-            <div class="col-12 col-sm-6 col-lg-3">
-                <div class="single-team-member-area mb-100">
-                    <div class="team-thumb">
-                        <img src="<?php echo get_template_directory_uri().'/assets/img/bg-img/18.jpg'; ?>'; ?>" alt="">
-                        <!-- View More -->
-                        <div class="view-more">
-                            <a href="#">+</a>
-                        </div>
-                    </div>
-                    <div class="team-info">
-                        <h5>王婷婷4</h5>
-                        <h6>工业设计副总裁2019年</h6>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </div>
-</section>
+<?php
+get_template_part('assets/template/team','template');
+?>
 <!-- ##### Team Member Area End ##### -->
 
 <!-- ##### Miscellaneous Area Start ###### -->
