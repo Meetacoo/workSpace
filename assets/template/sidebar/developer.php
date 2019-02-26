37f7a8d196f1440689596c65ed4390960b81750a
HJF <2283785225@qq.com>
2019-02-26 15:51:56 +0800

    开发者中心sidebar

--- assets/template/sidebar/developer.php
+++ assets/template/sidebar/developer.php
@@ -7,45 +7,51 @@
  */
 ?>
 <?php
-$uri = 'http://localhost/wordpress/developerCenter';
+$uri = 'http://localhost/wordpress/developerCenter/';
 $args = array(
     array(
         'menu' => '概述和基础',
         'submenu' => array(
-            '产品介绍',
-            '商务接入流程',
-            '接入的产品能够解决怎样的问题',
-            '初步接入过程中需注意的关键点'
+            '商户接口',
+            '代收支付',
+            '快捷支付',
+            '移动支付',
+            '鉴权',
+            '网关'
         ),
-        'id' => array(
-            16, 17, 18
+        'href' => array(
+            $uri.'basic-merchant',
+            $uri.'basic-collectingpayment',
+            $uri.'basic-quickpay',
+            $uri.'basic-mpayment',
+            $uri.'basic-authentication',
+            $uri.'basic-gateway'
         ),
         'type' => 'dropdown'
     ),
     array(
         'menu' => '接入指南',
-        'id' => 11,
-        'href' => $uri.''
-    ),
-    array(
-        'menu' => '调试',
         'submenu' => array(
-            '参数的配置',
+            '商户接口',
+            '代收支付',
+            '快捷支付',
+            '移动支付',
+            '鉴权',
+            '网关'
         ),
-        'id' => array(
-            13
+        'href' => array(
+            $uri.'guide-merchant',
+            $uri.'guide-collectingpayment',
+            $uri.'guide-quickpay',
+            $uri.'guide-mpayment',
+            $uri.'guide-authentication',
+            $uri.'guide-gateway'
         ),
         'type' => 'dropdown'
     ),
     array(
-        'menu' => '论坛',
-        'submenu' => array(
-            '支付通道标准费用'
-        ),
-        'id' => array(
-            14
-        ),
-        'type' => 'dropdown'
+        'menu' => 'DEMO下载',
+        'href' => $uri.'demo',
     )
 );
 sideBarTemplate($args);
