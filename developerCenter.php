a3f030fa11c5407010cb244b9e8c4daf094d4bfe
HJF <2283785225@qq.com>
2019-02-26 15:41:23 +0800

    开发者中心

--- developerCenter.php
+++ developerCenter.php
@@ -6,12 +6,9 @@
 <?php
 get_template_part('assets/template/developerCenter/developer', 'header');
 
-$pageId = get_the_ID();
-$page = get_page($pageId);
-
 the_post();
 ?>
-    <div class="text-center head col-12" style="line-height: 50px">
+    <div class="text-center head col-12 mb-4" style="line-height: 50px">
         <?php echo get_the_title(); ?>
     </div>
 <?php
