3fb173c77b945ffb792895b39fa52e585de549e1
HJF <2283785225@qq.com>
2019-02-28 11:00:07 +0800

    优化帮助中心模板

--- help.php
+++ help.php
@@ -2,13 +2,9 @@
 /**
  * Template Name: 帮助中心模板
  */
-?>
-<?php
 get_template_part('assets/template/help/help', 'header');
 
 the_post();
-?>
-<?php
 echo the_content();
 
 get_template_part('assets/template/help/help', 'footer');
