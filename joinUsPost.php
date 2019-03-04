d82a8a88c38a5aee7a24613f094db3c23e3c07c2
HJF <2283785225@qq.com>
2019-03-04 10:27:32 +0800

    招聘职位简介

---
+++ joinUsPost.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: 黄金芳
+ * Date: 2019/3/1
+ * Time: 15:39
+ */
+/**
+ * Template Name: 加入我们招聘职位模板
+ */
+$uri = get_template_directory_uri();
+get_template_part('assets/template/joinUs/header');
+the_post();
+?>
+<div class="credit-tabs-content mt-50">
+    <div class="container">
+        <div class="text-center head col-12 mb-4" style="line-height: 50px">
+            <h5><?php echo get_the_title(); ?></h5>
+        </div>
+        <div style="padding: 100px 5%;" class="border rounded">
+            <?php echo the_content(); ?>
+        </div>
+
+    </div>
+</div>
+<?php
+get_template_part('assets/template/joinUs/footer');
+?>
