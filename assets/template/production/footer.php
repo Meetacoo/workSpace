476b4399d45c8b68d8c19804cafa7f12f92262db
HJF <2283785225@qq.com>
2019-03-04 11:45:24 +0800

    更改路径方式

--- assets/template/production/footer.php
+++ assets/template/production/footer.php
@@ -5,10 +5,7 @@
  * Date: 2019/2/27
  * Time: 18:59
  */
-?>
-
-<!-- ##### Footer Area Start ##### -->
-<?php
+$uri = get_template_directory_uri();
 get_template_part('assets/template/partner', 'template');
 get_template_part('assets/template/welcome/production');
 get_footer();
@@ -17,17 +14,17 @@ get_footer();
 
 <!-- ##### All Javascript Script ##### -->
 <!-- jQuery-2.2.4 js -->
-<script src="<?php echo get_template_directory_uri() . '/assets/js/jquery/jquery-2.2.4.min.js'; ?>"></script>
+<script src="<?php echo $uri. '/assets/js/jquery/jquery-2.2.4.min.js'; ?>"></script>
 <script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="<?php echo get_template_directory_uri() . '/assets/js/plugins/owl.carousel.min.js'; ?>"></script>
-<script src="<?php echo get_template_directory_uri() . '/assets/js/container/partner.js'; ?>"></script>
+<script src="<?php echo $uri. '/assets/js/plugins/owl.carousel.min.js'; ?>"></script>
+<script src="<?php echo $uri. '/assets/js/container/Partner.js'; ?>"></script>
 
 <!-- Popper js -->
-<script src="<?php echo get_template_directory_uri() . '/assets/js/bootstrap/popper.min.js'; ?>"></script>
+<script src="<?php echo $uri. '/assets/js/bootstrap/popper.min.js'; ?>"></script>
 <!-- All Plugins js -->
-<script src="<?php echo get_template_directory_uri() . '/assets/js/plugins/plugins.js'; ?>"></script>
+<script src="<?php echo $uri. '/assets/js/plugins/plugins.js'; ?>"></script>
 <!-- Active js -->
-<script src="<?php echo get_template_directory_uri() . '/assets/js/active.js'; ?>"></script>
+<script src="<?php echo $uri. '/assets/js/active.js'; ?>"></script>
 </body>
 
 </html>
